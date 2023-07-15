f873828cb106a9a9b8438a6b9ecb0eaa8656c980
Filip Paris <parisfilip2@gmail.com>
2023-07-15 11:08:49 +0200

    controller and requests for backend

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request) {
+
+    }
+    public function signup() {
+        
+    }
+    public function logout() {
+        
+    }
+}
